1b3454b6e931c5c27018119dd039d970de6010d5
Bohdan Yagginov <yagginov@gmail.com>
2024-06-04 21:50:53 +0300

    Initial commit

--- src/register_types.h
+++ src/register_types.h
@@ -1,7 +1,11 @@
-#ifndef EXAMPLE_REGISTER_TYPES_H
-#define EXAMPLE_REGISTER_TYPES_H
+#ifndef GDEXAMPLE_REGISTER_TYPES_H
+#define GDEXAMPLE_REGISTER_TYPES_H
 
-void initialize_gdextension_types();
-void uninitialize_gdextension_types();
+#include <godot_cpp/core/class_db.hpp>
 
-#endif // EXAMPLE_REGISTER_TYPES_H
+using namespace godot;
+
+void initialize_example_module(ModuleInitializationLevel p_level);
+void uninitialize_example_module(ModuleInitializationLevel p_level);
+
+#endif // GDEXAMPLE_REGISTER_TYPES_H
